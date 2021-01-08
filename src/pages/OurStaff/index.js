2c8a0a8e065f0e98347e0009d4621c38b50df544
Reza Yudhistira Gunawan <reza@toffin.id>
2021-01-08 20:57:18 +0700

    add function for get point

--- src/pages/OurStaff/index.js
+++ src/pages/OurStaff/index.js
@@ -1,34 +1,45 @@
-import React, {useEffect, useState} from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import {
   Gap,
   HomeProfile,
   NewsItem,
   OurStaffCategory,
   RatedOurStaff,
-} from '../../components';
-import {Fire} from '../../config';
-import {colors, fonts, showError, getData} from '../../utils';
-import {ILNullPhoto} from '../../assets';
+} from '../../components'
+import { Fire } from '../../config'
+import { colors, fonts, showError, getData } from '../../utils'
+import { ILNullPhoto } from '../../assets'
 
-const OurStaff = ({navigation}) => {
-  const [news, setNews] = useState([]);
-  const [categoryOurstaff, setCategoryOurstaff] = useState([]);
-  const [ourstaffs, setOurstaffs] = useState([]);
+const OurStaff = ({ navigation }) => {
+  const [news, setNews] = useState([])
+  const [categoryOurstaff, setCategoryOurstaff] = useState([])
+  const [ourstaffs, setOurstaffs] = useState([])
+  const [userPoint, setUserPoint] = useState([])
+
   const [profile, setProfile] = useState({
     photo: ILNullPhoto,
     fullName: '',
     profession: '',
-  });
+  })
 
   useEffect(() => {
-    getCategoryOurstaff();
-    getTopRatedOurstaffs();
-    getNews();
+    getCategoryOurstaff()
+    getTopRatedOurstaffs()
+    getNews()
     navigation.addListener('focus', () => {
-      getUserData();
-    });
-  }, [navigation]);
+      getUserData()
+    })
+  }, [navigation])
+
+  useEffect(() => {
+    Fire.auth().onAuthStateChanged((data) => {
+      console.log('verif -> data', data.uid)
+      if (data) {
+        getUserPoint(data.uid)
+      }
+    })
+  }, [])
 
   const getTopRatedOurstaffs = () => {
     Fire.database()
@@ -36,73 +47,92 @@ const OurStaff = ({navigation}) => {
       .orderByChild('rate')
       .limitToLast(3)
       .once('value')
-      .then(res => {
+      .then((res) => {
         if (res.val()) {
-          const oldData = res.val();
-          const data = [];
-          Object.keys(oldData).map(key => {
+          const oldData = res.val()
+          const data = []
+          Object.keys(oldData).map((key) => {
             data.push({
               id: key,
               data: oldData[key],
-            });
-          });
-          setOurstaffs(data);
+            })
+          })
+          setOurstaffs(data)
         }
       })
-      .catch(err => {
-        showError(err.message);
-      });
-  };
+      .catch((err) => {
+        showError(err.message)
+      })
+  }
 
   const getCategoryOurstaff = () => {
     Fire.database()
       .ref('category_ourstaff/')
       .once('value')
-      .then(res => {
+      .then((res) => {
         if (res.val()) {
-          const data = res.val();
-          const filterData = data.filter(el => el !== null);
-          setCategoryOurstaff(filterData);
+          const data = res.val()
+          const filterData = data.filter((el) => el !== null)
+          setCategoryOurstaff(filterData)
         }
       })
-      .catch(err => {
-        showError(err.message);
-      });
-  };
+      .catch((err) => {
+        showError(err.message)
+      })
+  }
 
   const getNews = () => {
     Fire.database()
       .ref('news/')
       .once('value')
-      .then(res => {
+      .then((res) => {
         if (res.val()) {
-          const data = res.val();
-          const filterData = data.filter(el => el !== null);
-          setNews(filterData);
+          const data = res.val()
+          const filterData = data.filter((el) => el !== null)
+          setNews(filterData)
         }
       })
-      .catch(err => {
-        showError(err.message);
-      });
-  };
+      .catch((err) => {
+        showError(err.message)
+      })
+  }
 
   const getUserData = () => {
-    getData('user').then(res => {
-      const data = res;
-      data.photo = res?.photo?.length > 1 ? {uri: res.photo} : ILNullPhoto;
-      setProfile(res);
-    });
-  };
+    getData('user').then((res) => {
+      const data = res
+      data.photo = res?.photo?.length > 1 ? { uri: res.photo } : ILNullPhoto
+      setProfile(res)
+    })
+  }
+
+  const getUserPoint = (uid) => {
+    Fire.database()
+      .ref('users/' + uid)
+      .on('value', (snapshot) => {
+        console.log(
+          '🚀 ~ file: index.js ~ line 29 ~ useEffect ~ snapshot',
+          snapshot.val(),
+        )
+        if (snapshot.val()) {
+          setUserPoint(snapshot.val())
+        }
+      })
+  }
   return (
     <View style={styles.page}>
       <View style={styles.content}>
         <ScrollView showsVerticalScrollIndicator={false}>
           <View style={styles.wrapperSection}>
             <Gap height={30} />
-            <HomeProfile
-              profile={profile}
-              onPress={() => navigation.navigate('UserProfile', profile)}
-            />
+            <View style={styles.row}>
+              <HomeProfile
+                profile={profile}
+                onPress={() => navigation.navigate('UserProfile', profile)}
+              />
+              <Text style={styles.point}>
+                Point Anda : {userPoint ? userPoint.point : 0}
+              </Text>
+            </View>
             <Text style={styles.welcome}>
               Silahkan memilih Staff dan Layanan Kami
             </Text>
@@ -111,7 +141,7 @@ const OurStaff = ({navigation}) => {
             <ScrollView horizontal showsHorizontalScrollIndicator={false}>
               <View style={styles.category}>
                 <Gap width={32} />
-                {categoryOurstaff.map(item => {
+                {categoryOurstaff.map((item) => {
                   return (
                     <OurStaffCategory
                       key={`category-${item.id}`}
@@ -120,7 +150,7 @@ const OurStaff = ({navigation}) => {
                         navigation.navigate('ChooseOurStaff', item)
                       }
                     />
-                  );
+                  )
                 })}
                 <Gap width={22} />
               </View>
@@ -128,22 +158,22 @@ const OurStaff = ({navigation}) => {
           </View>
           <View style={styles.wrapperSection}>
             <Text style={styles.sectionLabel}>Top Our Staff</Text>
-            {ourstaffs.map(ourstaff => {
+            {ourstaffs.map((ourstaff) => {
               return (
                 <RatedOurStaff
                   key={ourstaff.id}
                   name={ourstaff.data.fullName}
                   desc={ourstaff.data.profession}
-                  avatar={{uri: ourstaff.data.photo}}
+                  avatar={{ uri: ourstaff.data.photo }}
                   onPress={() =>
                     navigation.navigate('OurStaffProfile', ourstaff)
                   }
                 />
-              );
+              )
             })}
             <Text style={styles.sectionLabel}>Good News</Text>
           </View>
-          {news.map(item => {
+          {news.map((item) => {
             return (
               <NewsItem
                 key={`news-${item.id}`}
@@ -152,21 +182,30 @@ const OurStaff = ({navigation}) => {
                 date={item.date}
                 image={item.image}
               />
-            );
+            )
           })}
           <Gap height={30} />
         </ScrollView>
       </View>
     </View>
-  );
-};
+  )
+}
 
-export default OurStaff;
+export default OurStaff
 
 const styles = StyleSheet.create({
   page: {
     backgroundColor: colors.secondary,
     flex: 1,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  point: {
+    fontSize: 14,
+    color: '#E5B654',
   },
   content: {
     backgroundColor: colors.white,
@@ -174,7 +213,7 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
   },
-  wrapperSection: {paddingHorizontal: 16},
+  wrapperSection: { paddingHorizontal: 16 },
   welcome: {
     fontSize: 20,
     fontFamily: fonts.primary[600],
@@ -183,8 +222,8 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     maxWidth: 209,
   },
-  category: {flexDirection: 'row'},
-  wrapperScroll: {marginHorizontal: -16},
+  category: { flexDirection: 'row' },
+  wrapperScroll: { marginHorizontal: -16 },
   sectionLabel: {
     fontSize: 16,
     fontFamily: fonts.primary[600],
@@ -192,4 +231,4 @@ const styles = StyleSheet.create({
     marginTop: 30,
     marginBottom: 16,
   },
-});
+})
